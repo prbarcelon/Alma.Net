55b0475abdd077f145ed90575b435111c288454e
Philip Barcelon <prbarcelon@utexas.edu>
2024-07-28 05:14:10 -0500

    Added operators, clamping, and check for overflow or underflow.

--- src/AlmaNet/FloatNegativeOneToPositiveOne.cs
+++ src/AlmaNet/FloatNegativeOneToPositiveOne.cs
@@ -4,22 +4,35 @@ namespace AlmaNet
 {
     public readonly struct FloatNegativeOneToPositiveOne
     {
-        public FloatNegativeOneToPositiveOne(float value)
+        public FloatNegativeOneToPositiveOne(float value, bool clamp = false)
         {
-            if (value < -1.0f || value > 1.0f)
+            if (clamp)
+            {
+                if (value < -1.0f)
+                    value = -1.0f;
+                else if (value > 1.0f)
+                    value = 1.0f;
+            }
+            else if (value < -1.0f || value > 1.0f)
+            {
                 throw new ArgumentOutOfRangeException(nameof(value));
+            }
 
             Value = value;
         }
 
         public float Value { get; }
 
-        public static implicit operator FloatNegativeOneToPositiveOne(float value)
+        public static implicit operator FloatNegativeOneToPositiveOne(
+            float value
+        )
         {
             return new FloatNegativeOneToPositiveOne(value);
         }
 
-        public static implicit operator float(FloatNegativeOneToPositiveOne value)
+        public static implicit operator float(
+            FloatNegativeOneToPositiveOne value
+        )
         {
             return value.Value;
         }
--- src/AlmaNet/Personality/OceanModel.cs
+++ src/AlmaNet/Personality/OceanModel.cs
@@ -2,9 +2,13 @@
 {
     public readonly struct OceanModel
     {
-        public OceanModel(FloatNegativeOneToPositiveOne openness, FloatNegativeOneToPositiveOne conscientiousness,
-            FloatNegativeOneToPositiveOne extroversion, FloatNegativeOneToPositiveOne agreeableness,
-            FloatNegativeOneToPositiveOne neuroticism)
+        public OceanModel(
+            FloatNegativeOneToPositiveOne openness,
+            FloatNegativeOneToPositiveOne conscientiousness,
+            FloatNegativeOneToPositiveOne extroversion,
+            FloatNegativeOneToPositiveOne agreeableness,
+            FloatNegativeOneToPositiveOne neuroticism
+        )
         {
             Openness = openness;
             Conscientiousness = conscientiousness;
@@ -19,9 +23,37 @@
         public FloatNegativeOneToPositiveOne Agreeableness { get; }
         public FloatNegativeOneToPositiveOne Neuroticism { get; }
 
+        public static OceanModel operator *(OceanModel left, float right)
+        {
+            return new OceanModel(
+                Clamp(left.Openness * right),
+                Clamp(left.Conscientiousness * right),
+                Clamp(left.Extroversion * right),
+                Clamp(left.Agreeableness * right),
+                Clamp(left.Neuroticism * right)
+            );
+        }
+
+        public static OceanModel operator +(OceanModel left, OceanModel right)
+        {
+            return new OceanModel(
+                Clamp(left.Openness + right.Openness),
+                Clamp(left.Conscientiousness + right.Conscientiousness),
+                Clamp(left.Extroversion + right.Extroversion),
+                Clamp(left.Agreeableness + right.Agreeableness),
+                Clamp(left.Neuroticism + right.Neuroticism)
+            );
+        }
+
         public override string ToString()
         {
-            return $"O:{Openness}, C:{Conscientiousness}, E:{Extroversion}, A:{Agreeableness}, N:{Neuroticism}";
+            return
+                $"O:{Openness}, C:{Conscientiousness}, E:{Extroversion}, A:{Agreeableness}, N:{Neuroticism}";
+        }
+
+        private static FloatNegativeOneToPositiveOne Clamp(float value)
+        {
+            return new FloatNegativeOneToPositiveOne(value, true);
         }
     }
 }
